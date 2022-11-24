fc238e4456e8dee5391d6a6940344416d4decb86
Ryan <ryantruong927@gmail.com>
2022-11-24 01:24:20 -0500

    Testing url bug fix

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ class Page extends React.Component {
                     <h1>TMS</h1>
                     <p>A tool for receiving user feedback.</p>
                     <h1>Followed Companies:</h1>
-                    <a href="/company.html">Google</a>
+                    <a href="company.html">Google</a>
                     <p></p>
                 </div>
             </div>
